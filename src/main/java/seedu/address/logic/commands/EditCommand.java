963ea9c98db8a74ca83d893a757e3b02aff17c5e
Paul Tan <pyokagan@gmail.com>
2019-02-23 22:27:27 +0800

    Messages: add format(Person) for formatting a Person for display
    Commands may sometimes print a user-friendly representation of a person.
    For example, when adding a person, we get:
    
        New person added: John Doe Phone: 12345 Email: john@doe.com Address: Dough Ave Tags:
    
    The user-friendly representation (`John Doe Phone: 12345 Email...`) is
    formatted using Person#toString(). This is not very ideal because it
    means that the model is dealing with user-interface-related concerns,
    which it should not be doing because that is the responsibility of the
    logic and ui layers. Furthermore, using the toString() method for this
    task means that Person#toString() can't be used for more useful
    endeavours such as providing information for debugging purposes.
    
    As such, let's introduce Messages#format(Person) which would be a common
    method for formmating a Person for display to the user, and teach
    commands that need that functionality to use it.
    
    Co-authored-by: Zhaoqi <e0725180@u.nus.edu>

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -84,7 +84,7 @@ public class EditCommand extends Command {
 
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPerson));
+        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson)));
     }
 
     /**
