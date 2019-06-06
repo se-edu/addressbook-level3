a90c35afb6ffecce8acd1e25dbeaa7db6b99fee2
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2019-06-06 14:01:33 +0800

    Logic: Teach the app to accept 'remark' but do nothing
    Add a RemarkCommand that just throws an Exception.
    
    Add RemarkCommandTest that will test that executeUndoableCommand()
    throws an Exception.
    
    Modify AddressBookParser to accept a RemarkCommand.
    
    Add new test to AddressBookParserTest, which will test that typing
    "remark" returns an instance of RemarkCommand.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -15,6 +15,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -58,6 +59,9 @@ public class AddressBookParser {
 
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
+
+        case RemarkCommand.COMMAND_WORD:
+            return new RemarkCommand();
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
