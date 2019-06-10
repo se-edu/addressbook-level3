8ae9e458fed30aeef4cc7c28a2503881f1ff5322
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2019-06-10 14:37:47 +0800

    Storage: Add Remark field to JsonAdaptedPerson class
    Update tests and test data to use the remark field.
    
    This commit has been significantly modified when rebased in October 2024.
    * Fix json data to use "tags" instead of "tagged" as key
    * Fix JsonAdaptedPerson to use tags as variable name

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -21,7 +21,7 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_REMARK = "";
+    public static final String DEFAULT_REMARK = "She likes aardvarks.";
 
     private Name name;
     private Phone phone;
