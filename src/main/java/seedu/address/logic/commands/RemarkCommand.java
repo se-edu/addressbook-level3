01190de9229c1f8eedb4a3b05ea13838aab4876f
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2019-07-19 15:27:21 +0800

    Logic: Implement minimal `remark` command
    Add a minimum viable implementation of the `remark` command. This
    implementation meant to be a bridging step between seeing a positive
    result and doing things the right way.
    
    Tests are not implemented because students are not expected to know how
    to write them yet.

---
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+/**
+ * Changes the remark of an existing person in the address book.
+ */
+public class RemarkCommand extends Command {
+
+    public static final String COMMAND_WORD = "remark";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult("Hello from remark");
+    }
+}
