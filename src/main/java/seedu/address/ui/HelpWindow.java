809bb79c3d8055acc187573b51475acb6c65a4f8
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2019-08-29 18:56:32 +0800

    Prevent HelpWindow from maximizing
    Address a regression where subsequent `help` commands will spawn a
    `HelpWindow` maximized, covering the screen.
    Disable the maximize button completely as it serves no purpose.

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -36,6 +36,7 @@ public class HelpWindow extends UiPart<Stage> {
         super(FXML, root);
         helpMessage.setText(HELP_MESSAGE);
         root.sizeToScene();
+        root.setResizable(false);
     }
 
     /**
