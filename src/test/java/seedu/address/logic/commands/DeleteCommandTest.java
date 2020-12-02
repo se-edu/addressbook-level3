3ff6ff43623b57eb2c6c6865612e7cf8d84f1b66
CodyChew <chewchengyap@gmail.com>
2020-12-02 20:01:11 +0800

    Remove unnecessary comments in test cases
    Irrelevant comments found in test cases may cause confusion to future developers.
    
    Let's remove mentions of 'UndoCommand' and 'RedoCommand' in comments.

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -19,7 +19,7 @@ import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 
 /**
- * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
+ * Contains integration tests (interaction with the Model) and unit tests for
  * {@code DeleteCommand}.
  */
 public class DeleteCommandTest {
