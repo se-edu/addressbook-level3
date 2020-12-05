f17b9da01cc441afa363b0f77d7ca8ad88cbd8dd
Noorul Azlina <>
2020-12-05 16:01:00 +0800

    BugFix in Edit Command
    The bug that was fixed was that the Edit Command allows for duplicate people to be added.

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -77,7 +77,8 @@ public class EditCommand extends Command {
         Person personToEdit = lastShownList.get(index.getZeroBased());
         Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor);
 
-        if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
+        //!personToEdit.isSamePerson(editedPerson) &&
+        if (!personToEdit.equals(editedPerson) && model.hasPerson(editedPerson)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
 
