578e8b68d9fa4be80be2f9be70f00ef816b0ab1f
CodyChew <chewchengyap@gmail.com>
2020-12-18 17:00:35 +0800

    Update isSamePerson() equality for Person class
    The person equality check returns true if the name or the email
    matches. The OR condition complicates the equality check done when
    editing a person as it assumes isSamePerson() to have a transitive
    relation.
    
    Alternative 1: Improve the way the equality check is done in the edit
    command to prevent the above bug. (PRs #68, #69)
    
    Alternative 2: Simplify the equality check by removing the OR condition.
    This is simpler but possibly too strict for the address book domain
    i.e., increases the risk of duplicate entries for the same person
    creeping into the address book
    
    We decided to go with the alternative 2 because,
    * It is important to keep the code base simple, to reduce the initial
    learning curve of the students at the start of their project
    * Students can introduce a more sophisticated equality check that
    matches their product domain later.
    
    Also, let's change the default values of PersonBuilder utility class.
    This is because AddCommandIntegrationTest uses the default name in
    PersonBuilder and expects it to build a valid person. In this case, the
    default name cannot be a name in a model which uses TypicalPersons.

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -31,27 +31,23 @@ public class PersonTest {
         // null -> returns false
         assertFalse(ALICE.isSamePerson(null));
 
-        // different phone and email -> returns false
-        Person editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).build();
-        assertFalse(ALICE.isSamePerson(editedAlice));
+        // same name, all other attributes different -> returns true
+        Person editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
+                .withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND).build();
+        assertTrue(ALICE.isSamePerson(editedAlice));
 
-        // different name -> returns false
+        // different name, all other attributes same -> returns false
         editedAlice = new PersonBuilder(ALICE).withName(VALID_NAME_BOB).build();
         assertFalse(ALICE.isSamePerson(editedAlice));
 
-        // same name, same phone, different attributes -> returns true
-        editedAlice = new PersonBuilder(ALICE).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
-                .withTags(VALID_TAG_HUSBAND).build();
-        assertTrue(ALICE.isSamePerson(editedAlice));
+        // name differs in case, all other attributes same -> returns false
+        Person editedBob = new PersonBuilder(BOB).withName(VALID_NAME_BOB.toLowerCase()).build();
+        assertFalse(BOB.isSamePerson(editedBob));
 
-        // same name, same email, different attributes -> returns true
-        editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withAddress(VALID_ADDRESS_BOB)
-                .withTags(VALID_TAG_HUSBAND).build();
-        assertTrue(ALICE.isSamePerson(editedAlice));
-
-        // same name, same phone, same email, different attributes -> returns true
-        editedAlice = new PersonBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND).build();
-        assertTrue(ALICE.isSamePerson(editedAlice));
+        // name has trailing spaces, all other attributes same -> returns false
+        String nameWithTrailingSpaces = VALID_NAME_BOB + " ";
+        editedBob = new PersonBuilder(BOB).withName(nameWithTrailingSpaces).build();
+        assertFalse(BOB.isSamePerson(editedBob));
     }
 
     @Test
