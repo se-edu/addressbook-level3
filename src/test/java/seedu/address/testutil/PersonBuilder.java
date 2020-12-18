22bb0df07c0e19301bd7fec9d46b1004068072a2
CodyChew <chewchengyap@gmail.com>
2020-12-18 17:00:35 +0800

    Update isSamePerson() equality for Person class
    The person equality check returns true if the name or the email
    matches. The OR condition complicates the equality check done when
    editing a person as it assumes isSamePerson() to have a transitive
    relation.
    
    Alternative 1: Improve the way the equality check is done in the edit
    command to prevent the above bug. (PRs #68, #69)
    
    Alternative 2: Simplify the equality check by removing the OR condition.
    This is simpler but possibly too strict for the address book domain
    i.e., increases the risk of duplicate entries for the same person
    creeping into the address book
    
    We decided to go with the alternative 2 because,
    * It is important to keep the code base simple, to reduce the initial
    learning curve of the students at the start of their project
    * Students can introduce a more sophisticated equality check that
    matches their product domain later.

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -16,9 +16,9 @@ import seedu.address.model.util.SampleDataUtil;
  */
 public class PersonBuilder {
 
-    public static final String DEFAULT_NAME = "Alice Pauline";
+    public static final String DEFAULT_NAME = "Amy Bee";
     public static final String DEFAULT_PHONE = "85355255";
-    public static final String DEFAULT_EMAIL = "alice@gmail.com";
+    public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
 
     private Name name;
