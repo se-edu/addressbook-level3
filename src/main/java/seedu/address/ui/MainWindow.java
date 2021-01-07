739fda0e5ff6baa411a06faa02936b1cbbe864c1
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2021-01-07 17:05:36 +0800

    Interdiff between v2 and v3

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import java.nio.file.AccessDeniedException;
 import java.util.logging.Logger;
 
-import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
@@ -12,7 +11,6 @@ import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
@@ -206,9 +204,5 @@ public class MainWindow extends UiPart<Stage> {
     private void handleAccessDeniedException(Exception e) {
         logger.info("Insufficient Permissions for file");
         resultDisplay.setFeedbackToUser(e.getMessage());
-
-        PauseTransition pause = new PauseTransition(Duration.seconds(3));
-        pause.setOnFinished(event -> handleExit());
-        pause.play();
     }
 }
