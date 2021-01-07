7148c86a78f544e22c4a5e871dd1782bb837f04e
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2021-01-07 17:05:36 +0800

    Improve error message if no write permission
    When there is no write permission for the data file,
    an AccessDeniedException will be shown in the error message
    to the user.
    
    Let's make the error message more user friendly by informing
    the user that there are insufficient permissions for the file.

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -188,7 +188,6 @@ public class MainWindow extends UiPart<Stage> {
 
             return commandResult;
         } catch (CommandException | ParseException e) {
-            logger.info("Invalid command: " + commandText);
             resultDisplay.setFeedbackToUser(e.getMessage());
             throw e;
         }
