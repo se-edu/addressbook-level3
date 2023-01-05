5c1115ac758fe58eb4f21d42d2a275afd9242bff
Lim Junxue <60027105+LimJunxue@users.noreply.github.com>
2023-01-05 13:47:42 +0800

    Change file not found log to file found
    As 'file not found' logs may be interpreted
    as errors, let's log when the file is found
    instead.

--- src/main/java/seedu/address/commons/util/JsonUtil.java
+++ src/main/java/seedu/address/commons/util/JsonUtil.java
@@ -60,9 +60,9 @@ public class JsonUtil {
         requireNonNull(filePath);
 
         if (!Files.exists(filePath)) {
-            logger.info("Json file " + filePath + " not found");
             return Optional.empty();
         }
+        logger.info("Json file " + filePath + " found");
 
         T jsonFile;
 
