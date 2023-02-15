ded733acfff55ce4d982eae90f6aed3379310909
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-15 17:14:06 +0800

    Fix checkstyle due to missing javadoc

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Accepts remark that is not empty
+     * @param remark String of a remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
