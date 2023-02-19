7e88e3fa089cf2d85c6fe8cc9ffd4e3a57c5caae
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-19 14:53:45 +0800

    Provide test for performance command

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -36,6 +36,8 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_REMARK_AMY = "Like skiing.";
     public static final String VALID_REMARK_BOB = "Favourite pastime: Eating";
+    public static final String VALID_PERFORMANCE_AMY = "1";
+    public static final String VALID_PERFORMANCE_BOB = "2";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
