616a871eb01a6d4e4369d38bcdab8fb324c72ca4
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-19 15:04:37 +0800

    Fix line longer than 120 checkstyle error

--- src/test/java/seedu/address/logic/commands/PerformanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PerformanceCommandTest.java
@@ -19,8 +19,8 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Performance;
+import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 /**
@@ -37,7 +37,8 @@ public class PerformanceCommandTest {
         Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
         Person editedPerson = new PersonBuilder(firstPerson).withRemark(REMARK_STUB).build();
 
-        PerformanceCommand performanceCommand = new PerformanceCommand(INDEX_FIRST_PERSON, new Performance(editedPerson.getPerformance().value));
+        PerformanceCommand performanceCommand = new PerformanceCommand(INDEX_FIRST_PERSON,
+                new Performance(editedPerson.getPerformance().value));
 
         String expectedMessage = String.format(PerformanceCommand.MESSAGE_ADD_PERFORMANCE_SUCCESS, editedPerson);
 
@@ -71,7 +72,8 @@ public class PerformanceCommandTest {
         Person editedPerson = new PersonBuilder(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()))
                 .withRemark(REMARK_STUB).build();
 
-        PerformanceCommand performanceCommand = new PerformanceCommand(INDEX_FIRST_PERSON, new Performance(editedPerson.getPerformance().value));
+        PerformanceCommand performanceCommand = new PerformanceCommand(INDEX_FIRST_PERSON,
+                new Performance(editedPerson.getPerformance().value));
 
         String expectedMessage = String.format(PerformanceCommand.MESSAGE_ADD_PERFORMANCE_SUCCESS, editedPerson);
 
@@ -84,7 +86,8 @@ public class PerformanceCommandTest {
     @Test
     public void execute_invalidPersonIndexUnfilteredList_failure() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-        PerformanceCommand performanceCommand = new PerformanceCommand(outOfBoundIndex, new Performance(VALID_PERFORMANCE_BOB));
+        PerformanceCommand performanceCommand = new PerformanceCommand(outOfBoundIndex,
+                new Performance(VALID_PERFORMANCE_BOB));
 
         assertCommandFailure(performanceCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
@@ -100,7 +103,8 @@ public class PerformanceCommandTest {
         // ensures that outOfBoundIndex is still in bounds of address book list
         assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
 
-        PerformanceCommand performanceCommand = new PerformanceCommand(outOfBoundIndex, new Performance(VALID_PERFORMANCE_BOB));
+        PerformanceCommand performanceCommand = new PerformanceCommand(outOfBoundIndex, 
+                new Performance(VALID_PERFORMANCE_BOB));
 
         assertCommandFailure(performanceCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
