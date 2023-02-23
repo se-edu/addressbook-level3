a908a0b5ac11137c40102865a396f348dcab3d5f
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-23 17:29:09 +0800

    Add non-functional barebone of javafx mainpage card structure

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -1,7 +1,11 @@
 package seedu.address.ui;
 
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.List;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
@@ -18,15 +22,49 @@ public class PersonListPanel extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
     @FXML
-    private ListView<Person> personListView;
+    private ListView<Person> personListViewLeftCol;
+    @FXML
+    private ListView<Person> personListViewMidCol;
+    @FXML
+    private ListView<Person> personListViewRightCol;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
     public PersonListPanel(ObservableList<Person> personList) {
         super(FXML);
-        personListView.setItems(personList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
+        //Split into different group later
+        int skip = 3;
+        //int size = personList.size();
+// Limit to carefully avoid IndexOutOfBoundsException
+        //int limit = size / skip + Math.min(size % skip, 1);
+
+//        ObservableList<Person> filteredPersonList = Stream.iterate(0, i -> i + skip)
+//                .limit(limit)
+//                .map(personList::get)
+//                .collect(Collectors.toCollection(FXCollections::observableArrayList));
+
+        //Ensure no duplicate when skip especially if only have less than 6 etc
+        personListViewLeftCol.setItems(personList);
+        personListViewLeftCol.setCellFactory(listView -> new PersonListViewCell());
+
+        /*
+        filteredPersonList = Stream.iterate(1, i -> i + skip)
+                .limit(limit)
+                .map(personList::get)
+                .collect(Collectors.toCollection(FXCollections::observableArrayList));
+
+        personListViewMidCol.setItems(filteredPersonList);
+        personListViewMidCol.setCellFactory(listView -> new PersonListViewCell());
+
+        filteredPersonList = Stream.iterate(2, i -> i + skip)
+                .limit(limit)
+                .map(personList::get)
+                .collect(Collectors.toCollection(FXCollections::observableArrayList));
+
+        personListViewRightCol.setItems(filteredPersonList);
+        personListViewRightCol.setCellFactory(listView -> new PersonListViewCell());
+        */
     }
 
     /**
