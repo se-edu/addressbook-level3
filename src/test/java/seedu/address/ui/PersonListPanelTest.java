8456eca3a4469914ddcdaae133122a0a51e70f4b
ARPspoofing <mohdreyaaz99@gmail.com>
2023-03-01 09:45:18 +0800

    Fix test case import order

--- src/test/java/seedu/address/ui/PersonListPanelTest.java
+++ src/test/java/seedu/address/ui/PersonListPanelTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
