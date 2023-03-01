2a4ca8242321712fc7d9947c23c9061fcdd949d6
ARPspoofing <mohdreyaaz99@gmail.com>
2023-03-01 08:44:55 +0800

    Add test cases for gui settings

--- src/test/java/seedu/address/commons/core/GuiSettingsTest.java
+++ src/test/java/seedu/address/commons/core/GuiSettingsTest.java
@@ -8,48 +8,48 @@ import org.junit.jupiter.api.Test;
 public class GuiSettingsTest {
 
     @Test
-    public void getWindowWidth_defaultObject_widthReturned() {
+    public void windowWidth_defaultObject_widthReturned() {
         int defaultWidth = 800;
         assertEquals(defaultWidth, new GuiSettings().getWindowWidth());
     }
 
     @Test
-    public void getWindowHeight_defaultObject_HeightReturned() {
+    public void windowHeight_defaultObject_HeightReturned() {
         int defaultHeight = 900;
         assertEquals(defaultHeight, new GuiSettings().getWindowHeight());
     }
 
     @Test
-    public void getWindowCoordinates_defaultObject_NullReturned() {
+    public void windowCoordinates_defaultObject_NullReturned() {
         assertNull(new GuiSettings().getWindowCoordinates());
     }
 
     @Test
-    public void getImageViewHeight_defaultObject_HeightReturned() {
+    public void imageViewHeight_defaultObject_HeightReturned() {
         int defaultImageViewHeight = 35;
         assertEquals(defaultImageViewHeight, new GuiSettings().getImageViewHeight());
     }
 
     @Test
-    public void getImageViewWidth_defaultObject_WidthReturned() {
+    public void imageViewWidth_defaultObject_WidthReturned() {
         int defaultImageViewWidth = 40;
         assertEquals(defaultImageViewWidth, new GuiSettings().getImageViewWidth());
     }
 
     @Test
-    public void getCircleXCoordinates_defaultObject_XCoordinatesReturned() {
+    public void circleXCoordinates_defaultObject_XCoordinatesReturned() {
         int defaultCircleXCoordinates = 17;
         assertEquals(defaultCircleXCoordinates, new GuiSettings().getCircleX());
     }
 
     @Test
-    public void getCircleYCoordinates_defaultObject_YCoordinatesReturned() {
+    public void circleYCoordinates_defaultObject_YCoordinatesReturned() {
         int defaultCircleYCoordinates = 17;
         assertEquals(defaultCircleYCoordinates, new GuiSettings().getCircleY());
     }
 
     @Test
-    public void getCircleRadius_defaultObject_RadiusReturned() {
+    public void circleRadius_defaultObject_RadiusReturned() {
         int defaultCircleRadius = 17;
         assertEquals(defaultCircleRadius, new GuiSettings().getCircleRadius());
     }
