61c3abcfdc278fd2862eced5a6dca66f24fae8e2
ARPspoofing <mohdreyaaz99@gmail.com>
2023-03-04 10:36:04 +0800

    Resolve all comments and fixed all failing pipeline test cases

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -65,7 +65,6 @@ public class EditCommandParser implements Parser<EditCommand> {
         if (!editPersonDescriptor.isAnyFieldEdited()) {
             throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
         }
-
         return new EditCommand(index, editPersonDescriptor);
     }
 
