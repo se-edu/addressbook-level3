82188aa4df5ca99a5b865e67c3a49d8bf3488b29
ZQ <e0725180@u.nus.edu>
2023-05-12 21:26:14 +0800

    JsonAdaptedPerson: Change JSON property name tagged -> tags
    Similar to the previous commit, let's change the JSON property name
    'tagged' in the JsonAdaptedPerson class to 'tags'.

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -36,7 +36,7 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
             @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("tagged") List<JsonAdaptedTag> tags) {
+            @JsonProperty("tags") List<JsonAdaptedTag> tags) {
         this.name = name;
         this.phone = phone;
         this.email = email;
