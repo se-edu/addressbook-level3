a1ea21c90bfeb9c507c6f3739fbe43204a3380ea
ZQ <e0725180@u.nus.edu>
2023-06-07 07:38:43 +0800

    Removal of repeatable parameters
    The behavior of repeated parameters in AddCommand and EditCommand
    is to take the last value used.
    This can be confusing and hard to work with.
    Therefore, instead of allowing repeatable parameters,
    Let's change it to display a Command Error to the users instead.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -10,6 +10,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
@@ -43,6 +44,13 @@ public class AddCommandParser implements Parser<AddCommand> {
         Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+        Set<Prefix> duplicatePrefixes = argMultimap.getDuplicatePrefixes(PREFIX_NAME, PREFIX_PHONE,
+                PREFIX_EMAIL, PREFIX_ADDRESS);
+
+        if (!duplicatePrefixes.isEmpty()) {
+            throw new ParseException(Messages.formatDuplicatePrefixesToMessage(duplicatePrefixes));
+        }
 
         Person person = new Person(name, phone, email, address, tagList);
 
