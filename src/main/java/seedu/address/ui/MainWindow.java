da4ae36ddafbe76ac7def600b729b6d636672394
ZQ <e0725180@u.nus.edu>
2023-06-13 05:50:58 +0800

    Interdiff between v15 and v16

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -188,7 +188,7 @@ public class MainWindow extends UiPart<Stage> {
 
             return commandResult;
         } catch (CommandException | ParseException e) {
-            logger.info("An error occured while executing this command: " + commandText);
+            logger.info("An error occurred while executing command: " + commandText);
             resultDisplay.setFeedbackToUser(e.getMessage());
             throw e;
         }
