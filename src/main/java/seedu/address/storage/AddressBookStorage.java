5ccd0d625077fe87df8b2efea0502b448dd64f33
ZQ <e0725180@u.nus.edu>
2023-06-14 22:10:24 +0800

    Removal of unnecessary catch blocks and exceptions
    The handling of IOExceptions has been centralized within the
    JsonUtil.readJsonFile() method. As a result, catch blocks and
    IOExceptions in interfaces such as Storage, AddressBookStorage, and
    UserPrefsStorage are no longer necessary.
    
    Additionally, some logger messages and java documentations need to be
    updated to reflect this change.
    
    Therefore, let's remove the unused code and update the relevant log
    messages and documentations.

--- src/main/java/seedu/address/storage/AddressBookStorage.java
+++ src/main/java/seedu/address/storage/AddressBookStorage.java
@@ -19,16 +19,16 @@ public interface AddressBookStorage {
 
     /**
      * Returns AddressBook data as a {@link ReadOnlyAddressBook}.
-     *   Returns {@code Optional.empty()} if storage file is not found.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
      * @throws DataLoadingException if the data in storage is not in the expected format.
-     * @throws IOException if there was any problem when reading from the storage.
      */
-    Optional<ReadOnlyAddressBook> readAddressBook() throws DataLoadingException, IOException;
+    Optional<ReadOnlyAddressBook> readAddressBook() throws DataLoadingException;
 
     /**
      * @see #getAddressBookFilePath()
      */
-    Optional<ReadOnlyAddressBook> readAddressBook(Path filePath) throws DataLoadingException, IOException;
+    Optional<ReadOnlyAddressBook> readAddressBook(Path filePath) throws DataLoadingException;
 
     /**
      * Saves the given {@link ReadOnlyAddressBook} to the storage.
