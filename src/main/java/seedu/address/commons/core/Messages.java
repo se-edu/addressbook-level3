7f53e75e7c8b85d8492e60d370e11aa120f07b17
ZQ <e0725180@u.nus.edu>
2023-06-14 16:49:13 +0800

    Removal of repeatable parameters as a feature
    Currently, the behavior of repeated parameters in AddCommand and
    EditCommand is to consider the last value used. This behavior can be
    confusing and difficult to work with. Therefore, instead of allowing
    repeatable parameters, it is better to present them as a Command Error
    to the user.
    
    To implement this change, the checking for duplicate parameters should
    be performed before checking the validity of individual fields. In other
    words, if an Add or Edit Command contains multiple duplicate fields that
    are all invalid, the parsing error for duplicate fields will take precedence
    over other checks for individual parameters.

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -1,6 +1,10 @@
 package seedu.address.commons.core;
 
-import seedu.address.model.person.Person;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import seedu.address.logic.parser.Prefix;
 
 /**
  * Container for user visible messages.
@@ -11,22 +15,17 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_DUPLICATE_FIELDS =
+            "Multiple values specified for the following single-valued field(s): ";
 
     /**
-     * Formats the {@code person} for display to the user.
+     * Formats the duplicate prefixes into an error message.
      */
-    public static String format(Person person) {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(person.getName())
-                .append("; Phone: ")
-                .append(person.getPhone())
-                .append("; Email: ")
-                .append(person.getEmail())
-                .append("; Address: ")
-                .append(person.getAddress())
-                .append("; Tags: ");
-        person.getTags().forEach(builder::append);
-        return builder.toString();
-    }
+    public static String getDuplicatePrefixesToMessage(Prefix... duplicatePrefixes) {
+        assert duplicatePrefixes.length > 0;
 
+        Set<String> duplicateFields = Stream.of(duplicatePrefixes).map(Prefix::toString).collect(Collectors.toSet());
+
+        return MESSAGE_DUPLICATE_FIELDS + String.join(" ", duplicateFields);
+    }
 }
