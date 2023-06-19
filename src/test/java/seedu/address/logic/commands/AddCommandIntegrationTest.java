8d3049f7284c8cccaf761cbc1514e997d1d59188
lzq <li.zhaoqi@dhs.sg>
2023-06-19 10:34:39 +0800

    Messages: move to seedu.address.logic package
    The Messages class contains common user-facing messages used by commands
    and parsers in the logic layer.
    
    However, Messages is contained within the seedu.address.commons.core
    package, which is too general. Given that the Messages class is
    user-facing, it would only ever be used by the logic and ui layers, and
    thus would fit better inside the seedu.address.logic package.
    
    Furthermore, in the next few commits we will be adding methods to the
    Messages class which require access to model classes. This would not be
    possible if the Messages class is still in the commons layer, which is
    the lowest layer of our architecture and can't access component.
    
    As such, let's move the Messages class to the seedu.address.logic
    package.

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -7,7 +7,7 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.Messages;
+import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -16,7 +16,7 @@ import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.commons.core.Messages;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
