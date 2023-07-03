257b1bf13a1f0ae27e4bcf12c229fcfe662647aa
ZQ <e0725180@u.nus.edu>
2023-07-03 20:50:06 +0800

    Upgrade JavaFX to ver 17
    The application uses JavaFX 11 which is reaching its EOL in September
    2023. Thus it is better to upgrade JavaFX to the next LTS version which
    is version 17.
    
    However, after version 16, JavaFX now throws a warning when JavaFX
    modules are loaded from the classpath. As there is currently no plan to
    migrate to using Modules in AB3, let's include an additional log message
    to inform the users to ignore the warning message.

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -1,6 +1,9 @@
 package seedu.address;
 
+import java.util.logging.Logger;
+
 import javafx.application.Application;
+import seedu.address.commons.core.LogsCenter;
 
 /**
  * The main entry point to the application.
@@ -19,7 +22,23 @@ import javafx.application.Application;
  * to be the entry point of the application, we avoid this issue.
  */
 public class Main {
+    private static Logger logger = LogsCenter.getLogger(Main.class);
+
+    /**
+     * Main entry point to the application.
+     */
     public static void main(String[] args) {
+        /*
+         * Since JavaFX 16, JavaFX runtime logs a warning if javafx.* modules are loaded from the classpath.
+         * This will result in a warning message whenever the application is launched.
+         * As modules are ignored when packed into a FAT Jar file, this warning will always be shown regardless
+         * of whether the application is using modules or not.
+         *
+         * This warning however, can be safely ignored. Thus, this log is used to inform the user that the warning
+         * can be ignored.
+         * https://github.com/openjdk/jfx/blob/master/doc-files/release-notes-16.md
+         */
+        logger.info("The warning about Unsupported JavaFX configuration below can be ignored.");
         Application.launch(MainApp.class, args);
     }
 }
