61ee515ed74f471907d74347e664c9d1954038af
lzq <li.zhaoqi@dhs.sg>
2023-07-10 21:09:29 +0800

    Interdiff between v10 and v11

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -14,11 +14,10 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX =
-            "The person index provided is invalid";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
-            "Multiple values specified for the following single-valued field(s): ";
+                "Multiple values specified for the following single-valued field(s): ";
 
     /**
      * Formats the duplicate prefixes into an error message.
@@ -37,10 +36,16 @@ public class Messages {
      */
     public static String format(Person person) {
         final StringBuilder builder = new StringBuilder();
-        builder.append(person.getName()).append("; Phone: ").append(person.getPhone())
-                .append("; Email: ").append(person.getEmail()).append("; Address: ")
-                .append(person.getAddress()).append("; Tags: ");
+        builder.append(person.getName())
+                .append("; Phone: ")
+                .append(person.getPhone())
+                .append("; Email: ")
+                .append(person.getEmail())
+                .append("; Address: ")
+                .append(person.getAddress())
+                .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
     }
+
 }
