7841d2304ee410fa53e7a5522a67014689bf5f27
lzq <li.zhaoqi@dhs.sg>
2023-07-10 21:09:29 +0800

    Removal of repeatable parameters as a feature
    Currently, the behavior of repeated parameters in AddCommand and
    EditCommand is to consider the last value used. This behavior can be
    confusing and difficult to work with. Therefore, instead of allowing
    repeatable parameters, it is better to present them as a Command Error
    to the user.
    
    To implement this change, the checking for duplicate parameters should
    be performed before checking the validity of individual fields. In other
    words, if an Add or Edit Command contains multiple duplicate fields that
    are all invalid, the parsing error for duplicate fields will take precedence
    over other checks for individual parameters.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -38,6 +38,7 @@ public class AddCommandParser implements Parser<AddCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
         Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
         Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
