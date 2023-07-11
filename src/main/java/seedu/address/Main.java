c0b533276837a9859213ece00beaa8c5edcf1079
lzq <li.zhaoqi@dhs.sg>
2023-07-12 05:29:21 +0800

    Interdiff between v3 and v4

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -25,17 +25,17 @@ public class Main {
     private static Logger logger = LogsCenter.getLogger(Main.class);
 
     public static void main(String[] args) {
-        /*
-         * As per https://github.com/openjdk/jfx/blob/master/doc-files/release-notes-16.md
-         * JavaFX 16 (or later) runtime logs a warning at startup if JavaFX classes are loaded from
-         * the classpath instead of a module.
-         * Our application does not use Java modules yet. Even if it did, modules are ignored when
-         * packed into a FAT Jar file (as we do), which means this warning will persist even then.
-         * The warning however, can be safely ignored. Thus, the following log informs
-         * the user (if looking at the log output) that the said warning appearing in the log
-         * can be ignored.
-         */
-        logger.info("The warning about Unsupported JavaFX configuration below can be ignored.");
+
+        // As per https://github.com/openjdk/jfx/blob/master/doc-files/release-notes-16.md
+        // JavaFX 16 (or later) runtime logs a warning at startup if JavaFX classes are loaded from
+        // the classpath instead of a module.
+        // Our application does not use Java modules yet. Even if it did, modules are ignored when
+        // packed into a FAT Jar file (as we do), which means this warning will persist even then.
+        // The warning however, can be safely ignored. Thus, the following log informs
+        // the user (if looking at the log output) that the said warning appearing in the log
+        // can be ignored.
+
+        logger.warning("The warning about Unsupported JavaFX configuration below can be ignored.");
         Application.launch(MainApp.class, args);
     }
 }
