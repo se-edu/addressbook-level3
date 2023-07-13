926619d1f5df069a8f494c80dc24cb23521ee03b
lzq <li.zhaoqi@dhs.sg>
2023-07-13 08:11:27 +0800

    Removal of repeatable parameters as a feature
    If the same parameter is repeated multiple times in a command, all
    except the last occurrence are ignored silently. This can cause certain
    user errors to go unnoticed. Therefore, instead of allowing repeatable
    parameters, it is better to treat them as an error instead.
    
    Therefore, if any of the single-valued parameters are repeated more than
    once, a user error will be displayed to the user.
    
    # Conflicts:
    #	src/main/java/seedu/address/commons/core/Messages.java

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -42,7 +42,10 @@ public class EditCommandParser implements Parser<EditCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
 
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
+
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
             editPersonDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
         }
