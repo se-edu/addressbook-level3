2186ec5bb70ecbbef4e8eaa8aee5b23a114ec865
damithc <1673303+damithc@users.noreply.github.com>
2026-07-16 00:13:48 +0800

    Fix PersonCard article

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -10,7 +10,7 @@ import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * A UI component that displays information of a {@code Person}.
  */
 public class PersonCard extends UiPart<Region> {
 
