5f14a16d4a1b5a5e129773e2046967ecaf899196
damithc <1673303+damithc@users.noreply.github.com>
2026-07-16 00:13:53 +0800

    Correct PersonCard constructor Javadoc

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -42,7 +42,7 @@ public class PersonCard extends UiPart<Region> {
     private FlowPane tags;
 
     /**
-     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     * Creates a {@code PersonCard} with the given {@code Person} and index to display.
      */
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
