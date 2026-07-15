75ecac0cc024f6ed75b599191e22a6946295a17c
damithc <1673303+damithc@users.noreply.github.com>
2026-07-16 00:13:45 +0800

    Fix DeleteCommand possessive typo

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
 /**
- * Deletes a person identified using it's displayed index from the address book.
+ * Deletes a person identified using its displayed index from the address book.
  */
 public class DeleteCommand extends Command {
 
