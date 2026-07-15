6ed4c7b0a5bc8928f030d2e4ee5cc2593f5ea422
damithc <1673303+damithc@users.noreply.github.com>
2026-07-16 00:16:44 +0800

    End LogsCenter API sentence

--- src/main/java/seedu/address/commons/core/LogsCenter.java
+++ src/main/java/seedu/address/commons/core/LogsCenter.java
@@ -12,7 +12,7 @@ import java.util.logging.SimpleFormatter;
 
 /**
  * Configures and manages loggers and handlers, including their logging level.
- * Named {@link Logger}s can be obtained from this class<br>
+ * Named {@link Logger}s can be obtained from this class.<br>
  * These loggers have been configured to output messages to the console and a {@code .log} file by default,
  *   at the {@code INFO} level. A new {@code .log} file with a new numbering will be created after the log
  *   file reaches 5MB big, up to a maximum of 5 files.<br>
