b38057fe9e178e2a28ccdc9cd85cba2a12f699e8
damithc <1673303+damithc@users.noreply.github.com>
2026-07-16 00:21:43 +0800

    Fix Name constraint message grammar

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,7 +10,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain alphanumeric characters and spaces, and should not be blank";
 
     /*
      * The first character of the name must not be a whitespace,
